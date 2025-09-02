5456faba3936c4a2ce25c30b5f31d8c7e524e76d
Bhagya-SA <bhagyaanjaneya@gmail.com>
2025-09-03 05:28:44 +0530

    Create return_values.py

---
+++ Functions/return_values.py
@@ -0,0 +1,16 @@
+# return_values.py
+# Function returning values
+
+def add(a, b):
+    """This function returns the sum of two numbers."""
+    return a + b
+
+def square(n):
+    """This function returns the square of a number."""
+    return n * n
+
+# Using returned values
+result = add(5, 3)
+print("Sum:", result)
+
+print("Square of 4:", square(4))
