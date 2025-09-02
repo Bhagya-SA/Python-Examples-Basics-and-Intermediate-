51207be4d51c3eff3eb0841ada6020b2b5d4208e
Bhagya-SA <bhagyaanjaneya@gmail.com>
2025-09-03 05:24:53 +0530

    Create if_else.py

---
+++ ControlFlow/if_else.py
@@ -0,0 +1,11 @@
+# if_else.py
+# Example of if-elif-else statements
+
+num = int(input("Enter a number: "))
+
+if num > 0:
+    print("The number is Positive")
+elif num == 0:
+    print("The number is Zero")
+else:
+    print("The number is Negative")
