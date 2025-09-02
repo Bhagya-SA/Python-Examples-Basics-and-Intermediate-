d9fe7f208eff9979d552b97690dbb26aff2d9b26
Bhagya-SA <bhagyaanjaneya@gmail.com>
2025-09-03 05:27:38 +0530

    Create function_basics.py

---
+++ Functions/function_basics.py
@@ -0,0 +1,10 @@
+# functions_basic.py
+# Defining and calling a function
+
+def greet(name):
+    """This function prints a greeting message."""
+    print("Hello,", name)
+
+# Calling the function
+greet("Alice")
+greet("Bob")
