b5cdaca6e09bcc0ad3e4a5519d3cc079432181de
Bhagya-SA <bhagyaanjaneya@gmail.com>
2025-09-03 05:23:19 +0530

    Create input_output.py

---
+++ Basics/input_output.py
@@ -0,0 +1,12 @@
+# input_output.py
+# Taking input and displaying output
+
+# Taking input as string
+name = input("Enter your name: ")
+
+# Taking input as integer
+age = int(input("Enter your age: "))
+
+# Displaying output
+print("Hello,", name)
+print("You are", age, "years old.")
