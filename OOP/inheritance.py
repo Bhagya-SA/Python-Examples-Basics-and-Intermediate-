f50a1061179db3972ecc51a8f3e5f0879269bc96
Bhagya-SA <bhagyaanjaneya@gmail.com>
2025-09-03 05:35:57 +0530

    Create inheritance.py

---
+++ OOP/inheritance.py
@@ -0,0 +1,26 @@
+# inheritance.py
+# Example of Inheritance in Python
+
+# Parent class
+class Animal:
+    def speak(self):
+        print("This animal makes a sound.")
+
+# Child class
+class Dog(Animal):
+    def speak(self):
+        print("The dog barks.")
+
+# Child class
+class Cat(Animal):
+    def speak(self):
+        print("The cat meows.")
+
+# Using objects
+animal = Animal()
+dog = Dog()
+cat = Cat()
+
+animal.speak()
+dog.speak()
+cat.speak()
